bd1c27d58dc577123661705b6f2902fd2e9490e4
martoonio <18221011@std.stei.itb.ac.id>
2024-06-04 02:35:10 +0700

    refactor code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "179807137"
+    serviceWorkerVersion: "3821890806"
   }
 });
